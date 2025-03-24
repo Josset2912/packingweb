a579753b385b005e2c340fd370b38f2fcccc291e
jportuguez <jossetpr@gmail.com>
2025-03-24 12:56:50 -0500

    Actualizar el origen CORS en el servidor

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://santaazul.com", // Reemplaza con tu dominio frontend
+    origin: "https://santaazul.orender.com", // Reemplaza con tu dominio frontend
     methods: ["GET", "POST"],
     credentials: true, // Si usas autenticación
   },
